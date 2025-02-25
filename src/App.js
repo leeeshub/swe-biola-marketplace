811ead06dc9f732ce8a0853af939be9afe07f382
Hyoin Lee <hiliee09@naver.com>
2025-02-25 15:15:45 -0800

    feat(auth): Add SignUp component and route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from "./components/authentication/Login";
+import SignUp from "./components/authentication/SignUp";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
