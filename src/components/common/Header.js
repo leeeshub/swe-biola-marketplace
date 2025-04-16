0ac203ef0cf703623d10d9616364d6e7e6115489
ryan nyberg <ryan.nyberg@biola.edu>
2025-04-15 22:22:11 -0700

    Added sticky header, email chat button

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './common.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./common.css";
 
 const Header = () => {
   return (
-    <header className="p-4">
+    <header className="header p-4">
       <div className="container">
         <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-          <Link to="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-            <img src="/images/biola-logo.png" alt="Logo" width="200" className="me-4" />
+          <Link
+            to="/"
+            className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"
+          >
+            <img
+              src="/images/biola-logo.png"
+              alt="Logo"
+              width="200"
+              className="me-4"
+            />
           </Link>
 
           <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-            <li><Link to="/profile" className="nav-link px-2 me-4 tab">Profile</Link></li>
-            <li><Link to="/add-post" className="nav-link px-2 tab">Add Post</Link></li>
+            <li>
+              <Link to="/profile" className="nav-link px-2 me-4 tab">
+                Profile
+              </Link>
+            </li>
+            <li>
+              <Link to="/add-post" className="nav-link px-2 tab">
+                Add Post
+              </Link>
+            </li>
           </ul>
 
           <div className="text-end">
-            <Link to="/login" className="btn me-2 login-btn">Login</Link>
-            <Link to="/signup" className="btn signup-btn">Sign up</Link>
+            <Link to="/login" className="btn me-2 login-btn">
+              Login
+            </Link>
+            <Link to="/signup" className="btn signup-btn">
+              Sign up
+            </Link>
           </div>
         </div>
       </div>
