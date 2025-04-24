eb71192a4c8203597a3868b03c36780f6402dfae
Hyoin Lee <hiliee09@naver.com>
2025-04-24 13:47:39 -0700

    fix: routing problem

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainPage from './components/home/Main';
-import Login from "./components/authentication/Login";
-import SignUp from "./components/authentication/SignUp";
-import AddPost from './components/crud/AddPost';
-import EditPost from './components/crud/EditPost';
-import Info from './components/crud/Info';
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./AppRoutes";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<SignUp />} />
-        <Route path="add-post" element={<AddPost />} />
-        <Route path="update" element={<EditPost />} />
-        {/* need a post id for the update/info page */}
-        <Route path="info" element={<Info />} />
-      </Routes>
+      <AppRoutes />
     </BrowserRouter>
   );
 }
